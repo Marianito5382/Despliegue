45dd29be82bc642e110136a6a879dce6baa52d00
root <root@Mariano-pc-100101>
2024-05-26 20:36:14 -0600

    configurando puerto

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
@@ -13,6 +15,6 @@ async function bootstrap() {
 
   }));
   
-  await app.listen(3010);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
